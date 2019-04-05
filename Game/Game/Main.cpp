ca168408a23721e0bf6ea19162ffe9c10ea7507d
Dominik Rutter <dominik.rutter@gmail.com>
2019-04-04 20:28:48 -0400

    Main Loop with exit complete

--- Game/Game/Main.cpp
+++ Game/Game/Main.cpp
@@ -1,7 +1,28 @@
 // Includes and Usings
 #include <iostream>
 using std::cout; using std::cin;
+#include <conio.h>
+#include <chrono>
+#include <thread>
 // Entry Point
 int main() {
+	int time = 0;
+	char ch;
+	bool playing = true;
+	while (playing) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		time += 100;
+		cout << "Waited : " << time << "ms \n";
+		if (_kbhit()) {
+			ch = _getch();
+			if (ch == 27) {
+				playing = false;
+			}
+		}
+	}
+}
+
+
+void drawScreen() {
 
 }
