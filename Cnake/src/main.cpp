bbd33b15a45d82a306a7ffd4f4e03a67206621d7
Dominik Rutter <dominik.rutter@gmail.com>
2019-05-03 03:05:48 -0400

    Implement cube

--- Cnake/src/main.cpp
+++ Cnake/src/main.cpp
@@ -1,22 +1,50 @@
 // Proprocessor
 #include <SFML/Graphics.hpp> // SFML Graphics
-#include <conio.h>
 #include <iostream>
-#include <chrono>
-#include <thread>
 // Entry Point
 int main() {
 
-	sf::RenderWindow window(sf::VideoMode(1280, 720), "Cnake", sf::Style::Close | sf::Style::Titlebar); // Create Window
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "Cnake", sf::Style::Close | sf::Style::Resize); // Create Window
+	sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+	player.setFillColor(sf::Color::Cyan);
 
 	while (window.isOpen()) { // Game Loop
 
 		sf::Event evnt;
 		while (window.pollEvent(evnt)) { // Check all events since last iteration
-			if (evnt.type == sf::Event::Closed) { // "Close Event" occurs, close window
+			switch (evnt.type) {
+			
+			case sf::Event::Closed:
 				window.close();
+				break;
+			case sf::Event::Resized:
+				std::cout << "New window size (" << evnt.size.width << ", " << evnt.size.height << ")" << std::endl;
+			case sf::Event::TextEntered:
+				if (evnt.text.unicode < 128) {
+					std::cout << static_cast<float>(evnt.text.unicode) << '\n';
+				}
 			}
 		}
+		
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+			player.move(0.0f, -0.25f);
+		} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+			player.move(-0.25f, 0.0f);
+		} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+			player.move(0.0f, 0.25f);
+		}else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+			player.move(0.25f, 0.0f);
+		}
+
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+			sf::Vector2i mosPos = sf::Mouse::getPosition(window);
+			player.setPosition((float)mosPos.x, (float)mosPos.y);
+		}
+
+		window.clear();
+		window.draw(player);
+		window.display();
+
 	}
 }
 
