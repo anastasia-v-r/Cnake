2b78614f158a6910df84b7710028857116be5aac
Dominik Rutter <dominik.rutter@gmail.com>
2019-05-03 00:11:50 -0400

    Cleanup

--- Game/Game/input.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <vector>
-#include "instance.h"
-
-direction userInput(std::vector<std::vector<char>>&, char);
--- Game/Game/logic.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "instance.h"
-#include <vector>
-
-bool gameLogic(std::vector<bodyPiece>&, direction);
