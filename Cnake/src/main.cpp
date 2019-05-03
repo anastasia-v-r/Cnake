0a9bb968757da3987d5edb4dcad3943dad24242b
Dominik Rutter <dominik.rutter@gmail.com>
2019-05-03 02:38:34 -0400

    SFML Setup

--- Cnake/src/main.cpp
+++ Cnake/src/main.cpp
@@ -1,24 +1,22 @@
-#include <SFML/Graphics.hpp>
+// Proprocessor
+#include <SFML/Graphics.hpp> // SFML Graphics
+#include <conio.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
+// Entry Point
+int main() {
 
-int main()
-{
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "Cnake", sf::Style::Close | sf::Style::Titlebar); // Create Window
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
+	while (window.isOpen()) { // Game Loop
+
+		sf::Event evnt;
+		while (window.pollEvent(evnt)) { // Check all events since last iteration
+			if (evnt.type == sf::Event::Closed) { // "Close Event" occurs, close window
 				window.close();
+			}
 		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
 	}
-
-	return 0;
 }
+
