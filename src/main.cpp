05e07d508c432cf6dccb7e4c36995cc76db068cf
Dominik Rutter <dominik.rutter@gmail.com>
2019-05-13 03:34:26 -0400

    Introduce a systemclock, more doc

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // Proprocessor
 #include <SFML/Graphics.hpp> // SFML Graphics
 #include <iostream> // IO
+#include <iomanip> // IO Manipulation
 #include <string> // String
 #include <experimental/filesystem> // FilePath
 namespace filesystem = std::experimental::filesystem::v1; // FilePath
@@ -19,31 +20,50 @@ int main() {
 	//************
 	// Game Loop *
 	//************
+	sf::Clock gameClock; // Begin and create game clock
+	sf::Time tick = sf::milliseconds(200); // Divide game into 4 parts per second ticks
 	while (window.isOpen()) { 
-
+		//*****************
+		// Event Handeler *
+		//*****************
 		sf::Event evnt;
-		while (window.pollEvent(evnt)) { // Check all events since last iteration
-			switch (evnt.type) { 
-			
+		while (window.pollEvent(evnt)) { // Poll for events, if no event then skip 
+			switch (evnt.type)
+			{
 			case sf::Event::Closed: 
 				window.close();
 				break;
 			case sf::Event::Resized:
 				std::cout << "New window size (" << evnt.size.width << ", " << evnt.size.height << ")" << std::endl;
 				break;
-			case sf::Event::TextEntered:
-				if (evnt.text.unicode < 128) {
-					std::cout << static_cast<char>(evnt.text.unicode) << '\n';
+			//case sf::Event::TextEntered:
+			//	if (evnt.text.unicode < 128) {
+			//		std::cout << static_cast<char>(evnt.text.unicode) << '\n'; // Post key to console 
+			//	}
+			//	break;
+			case sf::Event::KeyPressed: 
+				switch (evnt.key.code) 
+				{
+				case sf::Keyboard::W:
+
+				case sf::Keyboard::A:
+				
+				case sf::Keyboard::S:
+				
+				case sf::Keyboard::D:
+					player.setDir(evnt.key.code); // Change Direction of snake
+					break;
+				case sf::Keyboard::Escape: // ESC Pressed
+					window.close(); // Close Window
+					break;
 				}
-				break;
-			case sf::Event::KeyPressed:
-				player.move(evnt.key.code);
-				sf::Vector2f* tempV2F = new sf::Vector2f;
-				*tempV2F = snake[0].getPosition();
-				float v2fX = tempV2F->x; float v2fY = tempV2F->y;
-				std::cout << v2fX << ", " << v2fY << std::endl;
-				delete tempV2F;
 			}
+		}
+		sf::Time elapsed = gameClock.getElapsedTime(); // Grab time elapsed 
+		//std::cout << elapsed.asSeconds();
+		if ((elapsed > tick) && (player.getDir() != STOP)) {
+			player.move(); // Move the player along the screen
+			gameClock.restart();
 		}
 
 		window.clear();
