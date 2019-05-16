6ad3a17ff3a3a82dc04423c4165f752835b95122
Dominik Rutter <dominik.rutter@gmail.com>
2019-05-16 19:18:21 -0400

    Fix

--- src/log.cpp
+++ src/log.cpp
@@ -1,7 +1,2 @@
 #include "log.h"
 #include <string>
-
-LogStream Log::Info{ Color::White, "INFO  | " };
-LogStream Log::Debug{ Color::Green, "DEBUG | " };
-LogStream Log::Warn{ Color::Blue,  "WARN  | " };
-LogStream Log::Error{ Color::Red,   "ERROR | " };
