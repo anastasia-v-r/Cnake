f301c8382bc94cf6d842023d4d7253f182c51336
Dominik Rutter <dominik.rutter@gmail.com>
2019-05-16 02:55:14 -0400

    Update OSX Support

--- src/fs.hpp
+++ src/fs.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
 #if __has_include(<filesystem>)
-#include <filesystem>
-namespace fs = std::filesystem;
+	#define FS_SUPPORT true
+	#include <filesystem>
+	namespace fs = std::filesystem;
 #elif __has_include(<experimental/filesystem>)
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
+	#define FS_SUPPORT true
+	#include <experimental/filesystem>
+	namespace fs = std::experimental::filesystem;
 #else
-#error "no filesystem support"
+	#define FS_SUPPORT false
+	#error "no filesystem support"
 #endif
