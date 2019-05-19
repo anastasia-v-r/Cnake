b3ecf52b807983d06203e977c497cb3928abf2cf
Dominik Rutter <dominik.rutter@gmail.com>
2019-05-19 00:56:38 -0400

    New Screen Class

--- src/screen.cpp
+++ src/screen.cpp
@@ -1,1 +1,8 @@
 #include "screen.h"
+#include <SFML/Graphics.hpp>
+
+void Screen::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+	for (auto& element : screenElements) {
+		target.draw(element, states);
+	}
+}
--- src/screen.h
+++ src/screen.h
@@ -6,4 +6,8 @@ class Screen : public sf::Drawable
 {
 public:
 	std::vector<sf::Drawable> screenElements;
+public:
+
+private:
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
