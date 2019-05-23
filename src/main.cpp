abb9b1be856d87ae7a2bd5e90fca54689f59f54e
Dominik Rutter <dominik.rutter@gmail.com>
2019-05-22 22:47:42 -0400

    Move towards screen buffer system

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,32 @@
-// Proprocessor
+// Preprocessor
 #include <SFML/Graphics.hpp> 
 #include <iostream> 
-#include <sstream> 
 #include <string> 
 #include "fs.hpp" 
-#include "player.h" 
-#include "screen.h"
+#include "player.hpp"
+#include "gameScreen.hpp"
+#include "pauseScreen.hpp"
+#include "startScreen.hpp"
+// Data
+enum GameState {
+	INTRO = 0,
+	PLAYING = 1,
+	PAUSED = 2
+};
 // Entry Point
 int main() {
 	// Path Check
 	#ifdef FS_SUPPORT
 		std::cout << fs::current_path().string() << std::endl;
 	#endif
-	void optionSwitch();
 	auto desktop = sf::VideoMode::getDesktopMode();
 	unsigned int sWidth = static_cast<unsigned int>(desktop.width), sHeight = static_cast<unsigned int>(desktop.height);
 	sf::RenderWindow window(sf::VideoMode(sWidth + 1, sHeight, desktop.bitsPerPixel), "Cnake", sf::Style::None);
-	std::vector<sf::Drawable> screenBuffer;
-	Screen mainMenu;
-	Screen pauseScreen;
-	Player playerSnake(sf::Color::Blue, sf::Color::Green, sWidth, sHeight); 
-	playerSnake.setPosition(sWidth, sHeight); 
-
-	sf::RectangleShape background(sf::Vector2f(sWidth, sHeight)); 
-	background.setFillColor(sf::Color(255, 255, 255, 50)); 
-	background.setOrigin(background.getSize().x / 2, background.getSize().y / 2); 
-	background.setPosition(sWidth / 2, sHeight / 2); 
-
-	sf::RectangleShape playBox(sf::Vector2f(sWidth - playerSnake.getSize().x, sHeight - playerSnake.getSize().x)); 
-	playBox.setFillColor(sf::Color::Transparent); 
-	playBox.setOutlineThickness(10.0f); 
-	playBox.setOutlineColor(sf::Color(255, 255, 255, 130)); 
-	playBox.setOrigin(playBox.getSize().x / 2, playBox.getSize().y / 2); 
-	playBox.setPosition(sWidth / 2, sHeight / 2);  
-
-	sf::Font bauh; 
-	if (!bauh.loadFromFile("../../../fonts/bauh.ttf")) {
-		std::cout << "Moving to second Directory" << std::endl;
-		if (!bauh.loadFromFile("../fonts/bauh.ttf")) {
-			std::cout << "Moving to second Directory" << std::endl;
-			if (!bauh.loadFromFile("fonts/bauh.ttf")) {
-				std::cout << "All directories exhausted for font bauh" << std::endl;
-			}
-		}
-	}
-
-	std::stringstream ss;
-	ss << "Score : " << playerSnake.getScore();
-	sf::Text scoreText(sf::String(ss.str()), bauh, 30);
-	scoreText.setOrigin(scoreText.getScale().x / 2, scoreText.getScale().y / 2);
-	scoreText.setPosition(sWidth / 50, (sHeight / 10) * 9);
-	scoreText.setColor(sf::Color::Red);
-
+	Player playerSnake(sf::Color::Blue, sf::Color::Green, sWidth, sHeight);
+	StartScreen* startScreen = new StartScreen(sWidth, sHeight);
+	GameScreen* gameScreen = new GameScreen(sWidth, sHeight, playerSnake.getSize().x);
+	std::vector<sf::Drawable*> screenBuffer{startScreen};
+	GameState gameState(PLAYING);
 	sf::Clock gameClock; 
 	sf::Time tick = sf::milliseconds(200); 
 
@@ -74,7 +49,16 @@ int main() {
 				}
 				break;
 			case sf::Event::KeyPressed:
-				if (!gamePaused) {
+				if (INTRO) {
+					switch (evnt.key.code)
+					{
+					case sf::Keyboard::Enter:
+						screenBuffer[0] = gameScreen;
+						screenBuffer.reserve(screenBuffer.size() + 1);
+						screenBuffer[1] = &playerSnake;
+						gameState = PLAYING;
+					}
+				} else if (PLAYING) {
 					switch (evnt.key.code)
 					{
 					case sf::Keyboard::W:
@@ -94,7 +78,6 @@ int main() {
 					case sf::Keyboard::Up:
 					case sf::Keyboard::S:
 					case sf::Keyboard::Down:
-						optionSwitch();
 						break;
 					case sf::Keyboard::Enter:
 
@@ -107,7 +90,7 @@ int main() {
 			}
 		}
 
-		if (!gamePaused) {
+		if (PLAYING) {
 			sf::Time elapsed = gameClock.getElapsedTime(); 
 			if ((elapsed > tick) && (playerSnake.getDir() != STOP)) {
 				playerSnake.move(); 
@@ -116,14 +99,10 @@ int main() {
 		}
 
 		window.clear();
-		for (auto& screenElement : screenBuffer) {
-			window.draw(screenElement);
+		for (const auto& screenElement : screenBuffer) {
+			window.draw(*screenElement);
 		}
 		window.display();
 		// sprite.getGlobalBounds().contains(mousePos) // storing here for later
 	}
-}
-
-void optionSwitch() {
-
 }
