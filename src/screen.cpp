1081d097b46ec47b5681f2c506144d5e8d576b4b
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-13 05:46:14 -0400

    Remove Deprecated screen.hpp/cpp

--- src/screen.cpp
+++
@@ -1,9 +0,0 @@
-#include "screen.hpp"
-#include <SFML/Graphics.hpp>
-#include <iostream>
-
-void Screen::draw(sf::RenderTarget& target, sf::RenderStates states) const {
-	for (const auto& element : screenElements) {
-		target.draw(*element, states);
-	}
-}
