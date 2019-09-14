d67fd9845e89c5db1e51a04c962d481314d13633
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-14 03:00:34 -0400

    Game Now Renders on a seperate Thread and game now has state logic

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <vector>
 #include <stack>
-#include <thread>
 #include "fs.hpp" 
 #include "player.hpp"
 #include "Mode.hpp"
@@ -12,34 +11,72 @@
 #include "MenuMode.hpp"
 #include "GameMode.hpp"
 // Func
-void RenderScreen(sf::RenderWindow&, std::vector<sf::RectangleShape>&);
+void RenderScreen(sf::RenderWindow* window) {
+	window->setActive(true);
+	sf::Clock RenderClock;
+	unsigned int frames = 0;
+	unsigned int FPS = 0;
+	sf::Font font;
+	font.loadFromFile("../../../fonts/bauh.ttf");
+	std::cout << "yah";
+	sf::Text FPSText(frames, font, 30);
+	while (true) {
+		window->clear();
+		window->draw(FPSText);
+		window->display();
+		frames++;
+		FPS = frames / RenderClock.getElapsedTime().asSeconds();
+		FPSText.setString(std::to_string(FPS));
+	}
+}
 // Entry Point
 int main() {
 	// Path Check
 	#ifdef FS_SUPPORT
 		std::cout << fs::current_path().string() << std::endl;
 	#endif
-	
 	auto desktop = sf::VideoMode::getDesktopMode();
-	desktop.width += 1.0f;
+	desktop.width += 1;
 	sf::RenderWindow window(desktop, "Cnake", sf::Style::None);
+	window.setActive(false);
 
-	std::vector<sf::RectangleShape> Elements;
+	std::vector<sf::RectangleShape> Elements{sf::RectangleShape(sf::Vector2f(10, 10))};
 	std::stack<Mode*> ModeStack;
-	
+	ModeStack.push(new IntroMode);
+
+	sf::Thread RenderThread(&RenderScreen, &window);
+	RenderThread.launch();
+
+	sf::Clock GameClock;
 	while (window.isOpen()) {
-
-	}
-
-
-}
-// Func
-void RenderScreen(sf::RenderWindow& window, std::vector<sf::RectangleShape>& ScreenElements) {
-	while (true) {
-		window.clear();
-		for (const auto& element : ScreenElements) {
-			window.draw(element);
+		sf::Time timePassed = GameClock.restart();
+		auto result = ModeStack.top()->Run(timePassed, window);
+		switch (result.first)
+		{
+		case ModeAction::None:
+			break;
+		case ModeAction::Add:
+			switch (result.second)
+			{
+			case ModeOption::Intro:
+				ModeStack.push(new IntroMode);
+				break;
+			case ModeOption::Menu:
+				ModeStack.push(new MenuMode);
+				break;
+			case ModeOption::Game:
+				ModeStack.push(new GameMode);
+				break;
+			case ModeOption::Paused:
+				// TODO: Decide how to pause the game
+				break;
+			}
+			break;
+		case ModeAction::Remove:
+			ModeStack.pop();
+			break;
 		}
-		window.display();
 	}
+
+
 }
