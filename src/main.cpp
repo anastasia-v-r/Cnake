ce80d19e79bc883b18d9cf0e750d7144618af180
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-14 01:02:28 -0400

    Introduce Rendering Thread

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 // Preprocessor
 #include <SFML/Graphics.hpp> 
 #include <iostream> 
-#include <string> 
+#include <string>
+#include <vector>
+#include <stack>
+#include <thread>
 #include "fs.hpp" 
 #include "player.hpp"
-// Data
-enum GameState {
-	INTRO = 0,
-	PLAYING = 1,
-	PAUSED = 2
-};
+#include "Mode.hpp"
+#include "IntroMode.hpp"
+#include "MenuMode.hpp"
+#include "GameMode.hpp"
+// Func
+void RenderScreen(sf::RenderWindow&, std::vector<sf::RectangleShape>&);
 // Entry Point
 int main() {
 	// Path Check
@@ -21,86 +24,22 @@ int main() {
 	desktop.width += 1.0f;
 	sf::RenderWindow window(desktop, "Cnake", sf::Style::None);
 
-	sf::RectangleShape Bg(sf::Vector2f(desktop.width, desktop.height));
-	Bg.setFillColor(sf::Color::Green);
-
-	Player playerSnake(sf::Color::Blue, sf::Color::Green, desktop.width, desktop.height);
-	GameState gameState(PLAYING);
+	std::vector<sf::RectangleShape> Elements;
+	std::stack<Mode*> ModeStack;
 	
-	sf::Clock gameClock; 
-	sf::Time tick = sf::milliseconds(200); 
-
-	bool gamePaused = false;
 	while (window.isOpen()) {
 
-		sf::Event evnt;
-		while (window.pollEvent(evnt)) { 
-			switch (evnt.type)
-			{
-			case sf::Event::Closed:
-				window.close();
-				break;
-			case sf::Event::LostFocus:
+	}
 
-			case sf::Event::TextEntered:
-				if (evnt.text.unicode < 128) {
-					char keyOut = static_cast<char>(evnt.text.unicode);
-					std::cout << "Keypressed : [" << keyOut << "]" << std::endl;  
-				}
-				break;
-			case sf::Event::KeyPressed:
-				if (INTRO) {
-					switch (evnt.key.code)
-					{
-					case sf::Keyboard::Enter:
-						gameState = PLAYING;
-					}
-				} else if (PLAYING) {
-					switch (evnt.key.code)
-					{
-					case sf::Keyboard::W:
-					case sf::Keyboard::A:
-					case sf::Keyboard::S:
-					case sf::Keyboard::D:
-						playerSnake.setDir(evnt.key.code); 
-						break;
-					case sf::Keyboard::Escape: 
-						gamePaused = !gamePaused;
-						break;
-					}
-				} else {
-					switch (evnt.key.code)
-					{
-					case sf::Keyboard::W:
-					case sf::Keyboard::Up:
-					case sf::Keyboard::S:
-					case sf::Keyboard::Down:
-						break;
-					case sf::Keyboard::Enter:
 
-						break;
-					case sf::Keyboard::Z:
-						window.close();
-						break;
-					}
-				}
-			}
-		}
-
-		if (PLAYING) {
-			sf::Time elapsed = gameClock.getElapsedTime(); 
-			if ((elapsed > tick) && (playerSnake.getDir() != STOP)) {
-				playerSnake.move(); 
-				gameClock.restart();
-			}
-		}
-
+}
+// Func
+void RenderScreen(sf::RenderWindow& window, std::vector<sf::RectangleShape>& ScreenElements) {
+	while (true) {
 		window.clear();
-		window.draw(Bg);
-		if (gameState != GameState::INTRO) {
-			window.draw(playerSnake);
+		for (const auto& element : ScreenElements) {
+			window.draw(element);
 		}
 		window.display();
-		// sprite.getGlobalBounds().contains(mousePos) // storing here for later
 	}
 }
