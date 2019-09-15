589b2fda6cdc22f2c38b81d1e9058eeb8fee568c
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-15 01:38:24 -0400

    Rendering thread is now a lambda function

--- src/main.cpp
+++ src/main.cpp
@@ -10,40 +10,6 @@
 #include <IntroMode.hpp>
 #include <MenuMode.hpp>
 #include <GameMode.hpp>
-// Func
-void RenderScreen(sf::RenderWindow& window, std::vector<sf::RectangleShape>& screenElements) {
-	// Window Settings
-	window.setActive(true);
-	window.setFramerateLimit(5000);
-	// Stat Holders
-	sf::Clock RenderClock;
-	unsigned int totalFrames = 0;
-	sf::Time totalTime(sf::Time::Zero);
-	sf::Time elapsedTime(sf::Time::Zero);
-	// Text Setup
-	sf::Font font;
-	font.loadFromFile("assets/fonts/bauh.ttf");
-	sf::Text FPSCounter("", font, 30);
-	sf::Text FrameInterval("", font, 15);
-	FrameInterval.move(0, 30);
-	// Render Loop
-	while (true) {
-		// Rendering
-		window.clear();
-		for (const auto& element : screenElements) {
-			window.draw(element);
-		}
-		window.draw(FPSCounter);
-		window.draw(FrameInterval);
-		window.display();
-		// Stat Updates
-		elapsedTime = RenderClock.restart();
-		totalTime += elapsedTime;
-		totalFrames += 1;
-		FPSCounter.setString(std::to_string(static_cast<unsigned int>(totalFrames / totalTime.asSeconds())) + " FPS");
-		FrameInterval.setString(std::to_string(totalTime.asMicroseconds() / totalFrames) + "µs FrameTime");
-	}
-}
 // Entry Point
 int main() {
 	// Setup Window
@@ -52,13 +18,45 @@ int main() {
 	sf::RenderWindow window(desktop, "Cnake", sf::Style::None);
 	window.setActive(false);
 	// Prepare Screen Elements
-	std::vector<sf::RectangleShape> Elements{sf::RectangleShape(sf::Vector2f(10, 10))};
-	Elements[0].move(500, 500);
+	std::vector<sf::RectangleShape> screenElements{sf::RectangleShape(sf::Vector2f(10, 10))};
+	screenElements[0].move(500, 500);
 	// Prepare Stack
 	std::stack<Mode*> ModeStack;
 	ModeStack.push(new IntroMode);
 	// Create Rendering Thread
-	std::thread RenderThread(RenderScreen, std::ref(window), std::ref(Elements));
+	std::thread RenderThread([&window, &screenElements] {
+		// Window Settings
+		window.setActive(true);
+		window.setFramerateLimit(5000);
+		// Stat Holders
+		sf::Clock RenderClock;
+		unsigned int totalFrames = 0;
+		sf::Time totalTime(sf::Time::Zero);
+		sf::Time elapsedTime(sf::Time::Zero);
+		// Text Setup
+		sf::Font font;
+		font.loadFromFile("assets/fonts/bauh.ttf");
+		sf::Text FPSCounter("", font, 30);
+		sf::Text FrameInterval("", font, 15);
+		FrameInterval.move(0, 30);
+		// Render Loop
+		while (true) {
+			// Rendering
+			window.clear();
+			for (const auto& element : screenElements) {
+				window.draw(element);
+			}
+			window.draw(FPSCounter);
+			window.draw(FrameInterval);
+			window.display();
+			// Stat Updates
+			elapsedTime = RenderClock.restart();
+			totalTime += elapsedTime;
+			totalFrames += 1;
+			FPSCounter.setString(std::to_string(static_cast<unsigned int>(totalFrames / totalTime.asSeconds())) + " FPS");
+			FrameInterval.setString(std::to_string(totalTime.asMicroseconds() / totalFrames) + "µs FrameTime");
+		}
+	});
 	// Begin Game
 	sf::Clock GameClock;
 	while (window.isOpen()) {
