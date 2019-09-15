34d01e98839b0e3dbf7811b160cd2ef1054f69f4
Dominik Rutter <dominik.rutter@gmail.com>
2019-09-15 06:57:14 -0400

    Change GameMode::processKeys to processKeys to fix "Extra qualification" error

--- include/GameMode.hpp
+++ include/GameMode.hpp
@@ -8,7 +8,7 @@ public:
 	GameMode();
 	virtual std::pair<ModeAction, ModeOption> Run(sf::Time, sf::RenderWindow&) override;
 private:
-	void GameMode::processKeys(sf::Keyboard::Key, bool);
+	void processKeys(sf::Keyboard::Key, bool);
 private:
 	std::map<std::string, bool> mKeys;
 };
