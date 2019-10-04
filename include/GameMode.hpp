d389f907ce4242f26071a82fe38df1171baf9e2c
Dominik Rutter <dominik.rutter@gmail.com>
2019-10-04 16:27:21 -0400

    GameMode now loads its speed from gameSettings

--- include/GameMode.hpp
+++ include/GameMode.hpp
@@ -6,10 +6,11 @@
 class GameMode : public Mode
 {
 public:
-	GameMode(std::mutex* mutex);
+	GameMode(std::mutex*, float);
 	virtual std::pair<ModeAction, ModeOption> Run(sf::Time, sf::RenderWindow&) override;
 private:
 	void processKeys(sf::Keyboard::Key, bool);
 private:
 	Player mPlayer;
+	float gameSpeed;
 };
