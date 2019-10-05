9e2e7d25acade6beed1eae4c562d132b793e737c
Dominik Rutter <dominik.rutter@gmail.com>
2019-10-05 06:37:27 -0400

    Settings screen now implemented Framerate can be locked to 60fps and the games speed during play can be chosen aswell

--- include/SettingsMode.hpp
+++ include/SettingsMode.hpp
@@ -1,14 +1,17 @@
 #pragma once
 #include "Mode.hpp"
+#include "Settings.hpp"
 #include <SFML/Graphics.hpp>
 
 class SettingsMode : public Mode
 {
 public:
-	SettingsMode(std::mutex* mutex);
+	SettingsMode(std::mutex*, Settings*);
 	virtual std::pair<ModeAction, ModeOption> Run(sf::Time, sf::RenderWindow&) override;
 private:
 
 private:
-
+	Settings* settings;
+	bool fps;
+	float speed;
 };
